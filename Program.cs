e104c986d9d70a41619507e6ef835ba0d99c6e5d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-25 20:19:49 -0300

    exercisio da senha git hub

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+string senhaDigitada;
+string senhaEsperada = "123456";
+
+Console.Write("Digite sua senha..:");
+senhaDigitada = Console.ReadLine()!;
+
+if(senhaDigitada == senhaEsperada)
+{
+    // Sucesso no login
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Olá, usuário. Logado com sucesso");
+}
+else
+{
+    // Fracasso no login
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Senha inválida");
+}
+
+Console.ResetColor();
